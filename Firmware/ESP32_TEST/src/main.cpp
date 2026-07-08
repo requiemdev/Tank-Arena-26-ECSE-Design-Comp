88982001e06299e7dccdf0a77b49726e30b14e71
Maxmilian Sobr <msob973@aucklanduni.ac.nz>
2026-07-09 11:48:48 +1200

    Added test code for project 2 and reading ADC pin instead of using GPIO number.

--- Firmware/ESP32_TEST/src/main.cpp
+++ Firmware/ESP32_TEST/src/main.cpp
@@ -22,9 +22,9 @@ void setup() {
 // Runs repeatedly after setup() is called.
 void loop() {
   // PWM output
-  analogWrite(PIN_SERVO, 288); // 288 = 0.80 * 360
+  analogWrite(PIN_SERVO, 204); // 204 = 0.80 * 255
 
-  uint16_t analogReadValue = analogRead(PIN_VCOMP1);
+  uint16_t analogReadValue = analogRead(A7); // A7 = ADC1_7.
   Serial.printf("ADC Analog Read Value = %d\n", analogReadValue);
   Serial.printf("Value in mV: = %d\n", analogReadMilliVolts(PIN_VCOMP1));
   
